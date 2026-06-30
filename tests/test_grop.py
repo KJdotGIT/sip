dcadc3b3b920afc818fdf6396d19de6911cf1250
KJdotGIT <julienkgaffy@gmail.com>
2026-06-30 09:54:19 +0200

    x

--- tests/test_grop.py
+++ tests/test_grop.py
@@ -1,4 +1,9 @@
-from grop import *
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import grop
 
 def test_example():
     assert True
